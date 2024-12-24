46670e6092eee692199193f1bc94f58f7f982080
ali <alimb2002@yahoo.com>
2024-12-24 12:39:23 +0330

    architecture made

---
+++ src/features/counter/counterTypes.ts
@@ -0,0 +1,2 @@
+// src/features/counter/counterTypes.ts
+export const FETCH_COUNTER_DATA = "counter/fetchData";
---
+++ src/hooks/useCounter.ts
@@ -0,0 +1,18 @@
+// src/hooks/useCounter.ts
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store";
+import { increment, decrement } from "../features/counter/counterSlice";
+
+export const useCounter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state: RootState) => state.counter.value);
+
+  const incrementCounter = () => dispatch(increment());
+  const decrementCounter = () => dispatch(decrement());
+
+  return {
+    counter,
+    incrementCounter,
+    decrementCounter,
+  };
+};
